3de766840dbc46f0739a484a94c3664c4adf0c6f
t-hareldamti <t-hareldamti@microsoft.com>
2025-05-19 14:58:18 +0000

    printing

--- user/tournament.c
+++ user/tournament.c
@@ -8,11 +8,11 @@ void exit_error(char* msg, int id) {
 int main(void)
 {
     int tournament_id;
-    printf("Starting tournament\n");
-    if ((tournament_id = tournament_create(16)) == -1) exit_error("Tournament not created", 0);
-    if (tournament_acquire() == -1) exit_error("ID %d failed to acquire", tournament_id);
-    printf("Critical section for ID: %d\n", tournament_id);
-    if (tournament_release() == -1) exit_error("ID %d failed to release", tournament_id);
+    printf("Creating tournament\n");
+    if ((tournament_id = tournament_create(16)) == -1) exit_error("Tournament failed to create\n", 0);
+    if (tournament_acquire() == -1) exit_error("ID %d failed to acquire\n", tournament_id);
+    //printf("Critical section for ID: %d\n", tournament_id);
+    if (tournament_release() == -1) exit_error("ID %d failed to release\n", tournament_id);
     
     while (wait(0) != -1); // Wait for all children to finish to sync final message
     if (tournament_id == 0) { printf("Finished tournament\n"); }
